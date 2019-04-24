7d5b80d57206b084352271f0e7e3cc2a22efb082
zhengyang <1021606521@qq.com>
2019-04-24 20:30:10 +0800

    欧几里得游戏

---
+++ p1290.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: p1290.c
+	> Author:zhengyang 
+	> Mail:1021606521@qq.com 
+	> Created Time: 2019年04月24日 星期三 18时59分49秒
+ ************************************************************************/
+#include<stdio.h>
+#include<math.h>
+int swap (int  *a, int *b) {
+    int temp ;
+    temp = *a;
+    *a = *b;
+    *b=temp;
+}
+int ojld (int m, int n ,int p) {
+    if (m == n) return p;
+    if (m / n >= 2) return p;
+    else return ojld (n, m - n,p ^ 1 );
+}
+
+
+int main() {
+    int m,n,a;
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++) {
+        scanf("%d %d",&m,&n);
+        if (m < n) swap(&m, &n);
+        if (ojld(m, n ,0) == 0) printf("Stan wins\n");
+        else printf("Ollie wins\n");
+    }
+    return 0;
+}
